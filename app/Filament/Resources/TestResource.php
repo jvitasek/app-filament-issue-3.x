f42d4036f05ba28893e657298e327f9f2dd71333
Jakub Vitásek <vitasek@shoptet.cz>
2025-03-19 12:28:21 +0100

    Issue

---
+++ app/Filament/Resources/TestResource.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TestResource\Pages;
+use App\Filament\Resources\TestResource\RelationManagers;
+use App\Models\Test;
+use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextInputColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TestResource extends Resource
+{
+    protected static ?string $model = Test::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                DatePicker::make('date'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextInputColumn::make('date')->type('date')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTests::route('/'),
+            'create' => Pages\CreateTest::route('/create'),
+            'edit' => Pages\EditTest::route('/{record}/edit'),
+        ];
+    }
+}
